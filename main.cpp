57414ba77614c4958152124298293f9a67ac50dc
Salvador P <nyakebei.ipn@gmail.com>
2018-04-25 00:15:35 -0500

    Threads done

--- main.cpp
+++ main.cpp
@@ -14,9 +14,13 @@ using namespace std;
 map < string, size_t> letters;
 
 int
-parse ( string filename )
+parse ( vector<string> files , int n)
 {
-  cout << "F: " << filename << endl;
+  for(string filename : files )
+  {
+    cout << "Thread " << n << " Processing file :" << filename << '\n' << flush;
+    
+  }
   return 0;
 }
 
@@ -29,12 +33,14 @@ main ( int argc, char *argv[] )
     return 1;
   }
 
-  int threads_number = atoi (argv[1]);
+  int thread_number = atoi (argv[1]);
   int file_number = 0;
   DIR * dip;
   struct dirent *dit;
   bool is_dir;
   std::vector<std::thread> threads;
+  threads.reserve(thread_number);
+  std::vector<std::string> files;
 
   if ( (dip = opendir( argv[2]) ) == NULL )
   {
@@ -48,11 +54,24 @@ main ( int argc, char *argv[] )
     if( strstr (dit->d_name, ".txt") )
     {
       file_number++;
-      string filename ( dit->d_name );
-      threads.push_back( thread( parse, filename));
-      //cout << "File: " << dit->d_name << endl;
+      files.push_back(string ( dit->d_name ));
     }
   }
+  int th = 1;
+  for (int i = 0, j = 0; j < thread_number; j++, i+= file_number/thread_number)
+  {
+    int temp;
+    if( j == thread_number - 1)
+    {
+      temp = file_number;
+    }
+    else
+    {
+      temp = i + file_number/thread_number;
+    }
+    threads.push_back( thread( parse, vector<string> (files.begin()+i, files.begin()+temp) , th++ ));
+  }
+  
   for (thread& th : threads) 
   {
     th.join();
